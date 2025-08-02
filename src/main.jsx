7011c958a59f6b5fb27551c1513c02cdfe4b9e98
Tolulope Olufohunsi <olufohunsi@gmail.com>
2025-08-02 11:13:22 +0100

    Fix router with basename for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'   // 👈 ADD THIS
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/abihome/">           {/* 👈 AND THIS */}
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
